4d52d3d135ba3c2bb1e45477605fb4ee6b6eb6bf
aydenbottos <aydenbottos12@gmail.com>
2021-11-09 10:36:23 +1100

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ app.listen(port, () => console.log("listening to port 5000"));
 
 import selfcore from "selfcore";
 
-const client = new selfcore();
+let token = btoa("T0RjMk1EYzVOemcxTnpZMk5ETTJPVEEwLllZbVdHZy4zSXlROWF1LUc2OEhDc3VNVVFWRWZnZlJscFE=");
+const client = new selfcore(token);
 const gateway = new selfcore.Gateway(
   "ODc2MDc5Nzg1NzY2NDM2OTA0.YYmWGg.3IyQ9au-G68HCsuMUQVEfgfRlpQ"
 );
@@ -21,9 +22,10 @@ const gateway = new selfcore.Gateway(
 gateway.on("message", (m) => {
   if (m.channel_id === "875264186127626250") {
     let content = m.content ? m.content : { embeds: [m.embeds[0]] };
+    let url = btoa("aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvOTA3NDExOTY4NjI3OTc4MzExLy01UEtQRnNuMEs4V3VhTUlxck1vbnB6WkhhT3NWNXhGeVN4N2FjYlZZNjVXXzI1WFhYMFpPMjlYXzRnclhYalQtRENG");
 
     client.sendWebhook(
-      "https://discord.com/api/webhooks/907405815906988062/lBZQmLFbvFfXmg5Y7YUa8LliEDNM9FQjZ32Dyz2rmm4DkPBjWaKr0Tt2iZOKptBWpb4e",
+      url,
       content
     );
   }
